88c7cff752038a969948fb238a3d491f26c9526f
kym僵尸 <kymjs123@gmail.com>
2014-01-03 21:02:12 +0800

    主界面Demo完成

---
+++ KJmusic/src/net/kymjs/music/utils/Player.java
@@ -0,0 +1,189 @@
+package net.kymjs.music.utils;
+
+import java.util.List;
+
+import net.kymjs.music.AppManager;
+import net.kymjs.music.Config;
+import net.kymjs.music.bean.Music;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.net.Uri;
+
+/**
+ * 播放器类 单例模式：封装了播放器的相关操作
+ * 
+ * @author kymjs
+ * 
+ */
+public class Player {
+    private static Player player = new Player();
+    private MediaPlayer media;
+
+    private int mode = Config.MODE_SEQUENCE;
+    private int playing = Config.PLAYING_STOP;
+
+    private List<Music> list;
+    private int position = 0;
+    private Context context;
+
+    private Player() {
+        list = ListData.getLocalList(AppManager.getAppManager()
+                .currentActivity());
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public List<Music> getList() {
+        return this.list;
+    }
+
+    public int getListPosition() {
+        return this.position;
+    }
+
+    public Music getMusic() {
+        return list.get(position);
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    public int getMode() {
+        return this.mode;
+    }
+
+    public int getPlaying() {
+        return playing;
+    }
+
+    public int getAudioSessionId() {
+        return media.getAudioSessionId();
+    }
+
+    // 获取播放的音乐文件总时间长度
+    public int getDuration() {
+        return media.getDuration();
+    }
+
+    // 获取当前播放音乐时间点
+    public int getCurrentPosition() {
+        return media.getCurrentPosition();
+    }
+
+    // 将音乐播放跳转到某一时间点,以毫秒为单位
+    public void seekTo(int msec) {
+        media.seekTo(msec);
+    }
+
+    public void destroy() {
+        media.release();
+        playing = Config.PLAYING_STOP;
+    }
+
+    public void stop() {
+        if (playing != Config.PLAYING_STOP) {
+            media.reset();
+            playing = Config.PLAYING_STOP;
+            context.sendBroadcast(new Intent(Config.RECEIVER_MUSIC_CHANGE));
+        }
+    }
+
+    public void pause() {
+        if (playing != Config.PLAYING_PAUSE) {
+            media.pause();
+            playing = Config.PLAYING_PAUSE;
+            context.sendBroadcast(new Intent(Config.RECEIVER_MUSIC_CHANGE));
+        }
+    }
+
+    // 正在暂停，即将开始继续播放
+    public Music replay() {
+        if (playing != Config.PLAYING_PLAY) {
+            media.start();
+            playing = Config.PLAYING_PLAY;
+        }
+        return list.get(position);
+    }
+
+    public Music play(Context context, List<Music> list, int position) {
+        // 如果有正在播放的歌曲，将它停止
+        if (playing == Config.PLAYING_PLAY) {
+            media.reset();
+        }
+        media = MediaPlayer.create(context,
+                Uri.parse("file://" + list.get(position).getPath()));
+        media.start();
+        this.list = list;
+        this.position = position;
+        this.context = context;
+        media.setOnCompletionListener(new OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Player.this.context.sendBroadcast(new Intent(
+                        Config.RECEIVER_MUSIC_CHANGE));
+                completion(Player.this.context, Player.this.list,
+                        Player.this.position);
+            }
+        });
+        playing = Config.PLAYING_PLAY;
+        context.sendBroadcast(new Intent(Config.RECEIVER_MUSIC_CHANGE));
+        return list.get(position);
+    }
+
+    public Music next(Context context) {
+        Music music = null;
+        if (list.size() < 1) {
+            this.destroy();
+        } else {
+            media.reset(); // 停止上一首
+            position = (position + 1) % list.size();
+            play(context, list, position);
+            music = list.get(position);
+        }
+        return music;
+    }
+
+    public Music previous(Context context) {
+        Music music = null;
+        if (list.size() < 1) {
+            this.destroy();
+            music = null;
+        } else {
+            media.reset(); // 停止上一首
+            position = (position + list.size() - 1) % list.size();
+            play(context, list, position);
+            music = list.get(position);
+        }
+        return music;
+    }
+
+    public Music completion(Context context, List<Music> list, int position) {
+        Music music = null;
+        switch (mode) {
+        case Config.MODE_REPEAT_SINGLE:
+            // 单曲播放
+            stop();
+            break;
+        case Config.MODE_REPEAT_ALL:
+            // 单曲循环
+            music = play(context, list, position);
+            break;
+        case Config.MODE_SEQUENCE:
+            // 列表循环
+            music = play(context, list, (position + 1) % list.size());
+            break;
+        case Config.MODE_RANDOM:
+            // 随机循环
+            music = play(context, list, (int) (Math.random() * list.size()));
+            break;
+        default:
+            break;
+        }
+        return music;
+    }
+}
---
+++ KJmusic/src/net/kymjs/music/utils/UIHelper.java
@@ -0,0 +1,26 @@
+package net.kymjs.music.utils;
+
+import net.kymjs.music.AppManager;
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 应用程序UI相关工具类
+ * 
+ * @author kymjs
+ * 
+ */
+public class UIHelper {
+    static Toast toast = null;
+
+    public static void toast(String msg) {
+        toast = Toast.makeText(AppManager.getAppManager().currentActivity(),
+                msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public static void toast(Context context, String msg) {
+        toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
