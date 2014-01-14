71a26ec3a57af6a62e07efbf9b93cc1288cf80fd
kymjs <kymjs@kymjs-pc>
2014-01-14 21:43:33 +0800

    错误处理工具类

---
+++ KJmusic/src/net/kymjs/music/utils/ErrHandleUtils.java
@@ -0,0 +1,14 @@
+package net.kymjs.music.utils;
+
+import net.kymjs.music.Config;
+import android.content.Context;
+import android.content.Intent;
+
+public class ErrHandleUtils {
+    public static void sendErrInfo(Context context, String errInfo) {
+        Intent errIntent = new Intent();
+        errIntent.setAction(Config.RECEIVER_ERROR);
+        errIntent.putExtra("error", errInfo);
+        context.sendBroadcast(errIntent);
+    }
+}
